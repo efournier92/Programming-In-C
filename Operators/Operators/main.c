2361c49b267455b237bfa39b236d87f37449ac5a
Misigno <efournier92@gmail.com>
2016-10-30 18:20:56 -0400

    add Operators project

---
+++ Operators/Operators/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Operators
+//
+//  Created by Misigno on 10/30/16.
+//  Copyright © 2016 efournier. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
