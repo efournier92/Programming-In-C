105c55fa1285650176cc1027bd54ee05c417c57d
Misigno <efournier92@gmail.com>
2016-10-30 19:14:55 -0400

    finish Operators project

--- Operators/Operators/main.c
+++ Operators/Operators/main.c
@@ -13,14 +13,21 @@ int main(int argc, const char * argv[]) {
     float tempF, tempC;
     tempF=27;
     tempC = (tempF-32.)/1.8;
-    printf("%.2f\n", tempC);
+    printf("27°F = %.2f°C\n", tempC);
     
-    // Evaluate polynomial (3x3 + 5x2 – 6) for x=2.55
     
-    // Evaluate and print out the result of ( 2.31 x 106  x  2.887 x 10-5 ) / ( 7.16 x 10-4  +  6.09 x 103 )
+    // Evaluate polynomial 3x^3 + 5x^2 – 6 for x=2.55
+    float ans, x;
+    x=2.55;
+    ans=(3 * pow(x,2)) + (5 * pow(x,2) - 6);
+    printf("3x^3 + 5x^2 – 6 = %.2f\n", ans);
+    
+    
+    // Evaluate and print out the result of 2.31 x 10^6  x  2.887 x 10^-5 ) / ( 7.16 x 10^-4  +  6.09 x 10^3
     float sol;
-    sol=(2.31 * pow(10, 6) * 2.887 * pow(10, -5)) / ((7.16 * pow(10, -4)) + (6.09 * pow(10,3)));
-    printf("%.2f\n", sol);
+    sol=(2.31 * pow(10,6) * 2.887 * pow(10,-5)) / ((7.16 * pow(10,-4)) + (6.09 * pow(10,3)));
+    printf("Result = %.2f\n\n", sol);
+    
     
     // Take a four digit number and print out the ones digit, the tens digit, the hundreds digit, and the thousands digit.
     // Ese 5296 as an example
@@ -35,7 +42,7 @@ int main(int argc, const char * argv[]) {
     printf("     ONES = %i\n", ones);
     printf("     TENS = %i\n", tens);
     printf(" HUNDREDS = %i\n", hund);
-    printf("THOUSANDS = %i\n", thou);
+    printf("THOUSANDS = %i\n\n", thou);
     
     return 0;
 }
