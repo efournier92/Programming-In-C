9dd8ac81ccef920ceb3e10b7ac4e36b923b4223b
Misigno <efournier92@gmail.com>
2016-11-02 21:41:33 -0400

    finish input part 03

--- Input/Input/main.c
+++ Input/Input/main.c
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define PI (3.141592653589793)
 
 int main(int argc, const char * argv[]) {
     
@@ -16,12 +18,11 @@ int main(int argc, const char * argv[]) {
     printf("Please input 2 floating point numbers:\n>> ");
     scanf("%f %f", &float_1, &float_2);
     prod = float_1 * float_2;
-    printf("1st Number = %.4f\n2nd Number = %.4f\n   Product = %.4f\n", float_1, float_2, prod);
+    printf("1st Number = %.4f\n2nd Number = %.4f\n   Product = %.4f\n\n", float_1, float_2, prod);
     
     // Create a file and add any 10 floating point numbers, one per en the file and read in the ten numbers
     // Divide each number by 100.
     // Open a different file and write the divided numbers, one per line, in the reverse order.
-    
     FILE *inFile;
     FILE *outFile;
     size_t len = 0;
@@ -41,7 +42,16 @@ int main(int argc, const char * argv[]) {
     // Write logic to calculate the area of a circle and volume of a sphere.
     // Ask user for the radius, then calculate and output the circle area and the sphere volume.
     // Print the area and volume with three digits after the decimal.
-
+    float radius, circle_area, sphere_volume;
+    
+    printf("Please Input Radius:\n>> ");
+    scanf("%f", &radius);
+    circle_area = PI * pow(radius,2);
+    sphere_volume = (4.0/3) * PI * pow(radius,3);
+    printf("  CIRCLE AREA: %.2f\n", circle_area);
+    printf("SPHERE VOLUME: %.2f\n", sphere_volume);
+    
+    
     
     
     
