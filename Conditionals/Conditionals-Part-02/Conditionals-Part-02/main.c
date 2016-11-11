7ef19cf14be4b6a4d399d4202a6a7daece72335a
Misigno <efournier92@gmail.com>
2016-11-10 22:54:06 -0500

    add Roman Numeral Converter criteria

--- Conditionals/Conditionals-Part-02/Conditionals-Part-02/main.c
+++ Conditionals/Conditionals-Part-02/Conditionals-Part-02/main.c
@@ -1,15 +1,18 @@
-//
-//  main.c
-//  Conditionals-Part-02
-//
-//  Created by Misigno on 11/10/16.
-//  Copyright © 2016 efournier. All rights reserved.
-//
+// Conditionals-Part-02
+// efournier
+// Programming In C
 
 #include <stdio.h>
 
+// Write a program to convert base 10 integer numbers into Roman numerals
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // Ask for a number as input from the user
+    // Convert any number from 1 (I) to 3999 (MMMCMXCIX)
+    // Test to make sure that the number given is in that range.
+    // If it's not in range, tell the user and ask for another number.
+    // Ask if the user wants to convert another number.
+    
+    
+    
     return 0;
 }
