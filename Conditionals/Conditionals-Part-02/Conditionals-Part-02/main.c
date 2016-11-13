8daea9d1dcf17e3daf7dc95cbf2b4d403b73dba7
Misigno <efournier92@gmail.com>
2016-11-12 19:25:42 -0500

    start roman numeral logic

--- Conditionals/Conditionals-Part-02/Conditionals-Part-02/main.c
+++ Conditionals/Conditionals-Part-02/Conditionals-Part-02/main.c
@@ -6,8 +6,22 @@
 
 // Write a program to convert base 10 integer numbers into Roman numerals
 int main(int argc, const char * argv[]) {
-    // Ask for a number as input from the user
+    int num;
+    
+    // Ask for a number as input from the user.
+    printf("Please Input a Number: \n>> ");
+    scanf("%i", &num);
+    
     // Convert any number from 1 (I) to 3999 (MMMCMXCIX)
+    if (num > 4000) {
+        printf("Too Large to Convert");
+        while (num > 4000) {
+            printf("Input a Number Between 1 & 3999");
+        }
+    while (num > 1000) {
+    
+    }
+    
     // Test to make sure that the number given is in that range.
     // If it's not in range, tell the user and ask for another number.
     // Ask if the user wants to convert another number.
