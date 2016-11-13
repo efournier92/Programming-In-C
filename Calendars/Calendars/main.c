2629034d07294a609c5368ffa017edb9bb88fc9d
Misigno <efournier92@gmail.com>
2016-11-13 18:51:05 -0500

    add user input to Calendars-Question-01

--- Calendars/Calendars/main.c
+++ Calendars/Calendars/main.c
@@ -4,12 +4,19 @@
 
 #include <stdio.h>
 
-// Write a program to calculate the day of the week of the first day of any year.
+// Write a program to calculate the day of the week of the first day of any year
 int main(int argc, const char * argv[]) {
-
+    int year;
+    
     // Ask for the year (i.e. 2414)
+    printf("Please input a year\n>>");
+    scanf("%i", &year);
     
     // Check if year is valid (a positive integer year)
+    while (year <= 0) {
+        printf("Please input a positive integer year\n>>");
+        scanf("%i", &year);
+    }
     
     // If the year is not valid, tell user and ask for the year again
     
