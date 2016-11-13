3e4dc9957585d224c52ff10640ab61d90ff745a7
Misigno <efournier92@gmail.com>
2016-11-13 18:45:53 -0500

    add criteria to Calendars Part 01

--- Calendars/Calendars/main.c
+++ Calendars/Calendars/main.c
@@ -7,11 +7,13 @@
 // Write a program to calculate the day of the week of the first day of any year.
 int main(int argc, const char * argv[]) {
 
-
-// Ask for the year (i.e. 2414)
-// Check if year is valid (a positive integer year)
-// If the year is not valid, tell user and ask for the year again.
-//            Assume that the current calendar rules were constant since 1/1/1
+    // Ask for the year (i.e. 2414)
+    
+    // Check if year is valid (a positive integer year)
+    
+    // If the year is not valid, tell user and ask for the year again
+    
+    // Assume that the current calendar rules were constant since 1/1/1
 
     return 0;
 }
