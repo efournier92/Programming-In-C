40aa5bd4fef75ad695d932cb79e3d600f81ee736
Misigno <efournier92@gmail.com>
2016-11-12 20:53:03 -0500

    add string.h to roman numeral logic

--- Conditionals/Conditionals-Part-02/Conditionals-Part-02/main.c
+++ Conditionals/Conditionals-Part-02/Conditionals-Part-02/main.c
@@ -3,19 +3,21 @@
 // Programming In C
 
 #include <stdio.h>
+#include <string.h>
 
 // Write a program to convert base 10 integer numbers into Roman numerals
 int main(int argc, const char * argv[]) {
-    int num, i, j=0;
+
+    int num, i, j;
     int demoninations[7] = {1000, 500, 100, 50, 10, 5, 1};
     char numerals[7] = {'M', 'D', 'C', 'L', 'X', 'V', 'I'};
     char numeralString[10], repeat_input;
-    
+start:
+
     // Ask for a number as input from the user.
     printf("Please Input a Number: \n>> ");
     scanf("%i", &num);
     
-start:
     // Test to make sure that the number given is in that range
     if (num > 4000) {
         printf("Too Large to Convert");
@@ -37,9 +39,14 @@ start:
     
     // Ask if the user wants to convert another number
     printf("Want to go again? (Y/N)\n>> ");
-    scanf("%c", &repeat_input);
+    scanf("%s", &repeat_input);
     
-    if (repeat_input == 'Y' || repeat_input == 'y') goto start;
+    if (repeat_input == 'Y' || repeat_input == 'y') {
+        // Reset stored variables
+        memset(numeralString, 0, sizeof(numeralString));
+        j=0;
+        goto start;
+    }
     
     return 0;
 }
