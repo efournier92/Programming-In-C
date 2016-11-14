149e023d0c5a5f048a367675b4fe252f04a24ecb
Misigno <efournier92@gmail.com>
2016-11-13 19:34:20 -0500

    finish Calendars-Question-01 logic

--- Calendars/Calendars/main.c
+++ Calendars/Calendars/main.c
@@ -8,34 +8,38 @@
 // Assume that the current calendar rules were constant since 1/1/1
 int main(int argc, const char * argv[]) {
     int year, years_since, leap_years, century_years, four_century_years;
-    int total_leap_years, total_precesion, day_index;
-    char day_name;
-    const char *days[7];
+    int elapsed_years, total_leap_years, total_precesion, day_index;
+    const char *days[7], *is_was[1];
     days[0] = "Monday", days[1] = "Tuesday", days[2] = "Wednesday";
     days[3] = "Thursday", days[4] = "Friday", days[5] = "Saturday", days[6] = "Sunday";
     
     // Ask for the year (i.e. 2414)
-    printf("Please input a year\n>>");
+    printf("Please input a year\n>> ");
     scanf("%i", &year);
     
     // Check if year is valid (a positive integer year)
     while (year <= 0) {
         // If the year is not valid, tell user and ask for the year again
-        printf("Please input a positive integer year\n>>");
+        printf("Please input a positive integer year\n>> ");
         scanf("%i", &year);
     }
     
     years_since = year - 1;
-    leap_years = elapsed_years/4;
-    century_years = year/100;
-    four_century_years = year/400;
+    leap_years = years_since/4;
+    century_years = years_since/100;
+    four_century_years = years_since/400;
     
     total_leap_years = leap_years - century_years + four_century_years;
     total_precesion = year + total_leap_years;
     day_index = total_precesion % 7;
-    day_name = days[day_index];
     
-    printf("01-01-%i is a %s", year, day_name)
+    if (year < 2016) {
+        is_was[0] = "Started";
+    } else {
+        is_was[0] = "Will Start";
+    }
+    
+    printf("%i %s On A %s\n", year, is_was[0], days[day_index]);
     
     return 0;
 }
