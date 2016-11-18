a3221d13a3c5e2714b4973d9a4755af4cd04d03c
Misigno <efournier92@gmail.com>
2016-11-17 19:31:17 -0500

    Loops_Part-01 > start first loop

--- Loops/Loops_Part-01/Loops_Part-01/main.c
+++ Loops/Loops_Part-01/Loops_Part-01/main.c
@@ -5,8 +5,13 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
+    int i;
     
 // Count by 2’s from -33 to 10
+    for (i = -33; i > -33 && i <= 10) {
+    
+    }
+    
     
 // Count by 5’s from 243 to 288
     
