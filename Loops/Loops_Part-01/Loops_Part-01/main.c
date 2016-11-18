859d22606e695c60417517a428905acd1bdec209
Misigno <efournier92@gmail.com>
2016-11-17 19:21:09 -0500

    add project Loops_Part-01

---
+++ Loops/Loops_Part-01/Loops_Part-01/main.c
@@ -0,0 +1,21 @@
+// Loops_Part-01
+// efournier
+// Programming in C
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+// Count by 2’s from -33 to 10
+    
+// Count by 5’s from 243 to 288
+    
+// Count by -7’s from 133 to zero
+  
+//    Evaluate the function: y = x^3 - 4x^2 + 2x + 8
+
+//        Print this out in lines, each of which contains both the X value and the function value at the X value.
+
+    
+    return 0;
+}
