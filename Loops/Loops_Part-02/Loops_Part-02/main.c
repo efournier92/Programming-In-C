e3bdb7e6415d621fb418687770f3e5dbaea2455f
Misigno <efournier92@gmail.com>
2016-12-06 22:56:36 -0500

    add critia to Loops_Part-02 project

--- Loops/Loops_Part-02/Loops_Part-02/main.c
+++ Loops/Loops_Part-02/Loops_Part-02/main.c
@@ -1,12 +1,24 @@
-//
-//  main.c
 //  Loops_Part-02
-//
-//  Created by Misigno on 12/6/16.
-//  Copyright © 2016 efournier. All rights reserved.
-//
+//  efournier
+//  programming in C
 
 #include <stdio.h>
+
+//Write a program to count in arbitrary base
+//Ask the user for the base
+//Bases from 2 to 10 should be accepted
+//Count from 0 to the maximum number which can be written in 4 digits.
+//Strategy:
+//Use one variable to represent each digit
+//Always increment the least significant digit
+//Check for carrying one digit at a time (starting with the least significant digit)
+//Check to see if you have gotten to the top of the count.  (A good way is to have an extra digit and see if you carry into it.)
+
+//Write a program to print out a 1 month calendar for any month.
+//The program should ask for the  month in a year (i.e. 3/2134)
+//The program should check to make sure the date is valid
+//The format should look like a standard calendar which shows the days of the week in 1 row and aligns the dates for each day of the week in columns.
+//This is going to take a bit of work with output formatting.
 
 int main(int argc, const char * argv[]) {
     // insert code here...
