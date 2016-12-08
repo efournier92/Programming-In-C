9801d8f79b548a519c2058f2a4abee66eb938b8c
Misigno <efournier92@gmail.com>
2016-12-07 20:08:41 -0500

    Loops_Part-02 > add base input logic

--- Loops/Loops_Part-02/Loops_Part-02/main.c
+++ Loops/Loops_Part-02/Loops_Part-02/main.c
@@ -4,23 +4,29 @@
 
 #include <stdio.h>
 
-//Write a program to count in arbitrary base
-//Ask the user for the base
-//Bases from 2 to 10 should be accepted
-//Count from 0 to the maximum number which can be written in 4 digits.
-//Strategy:
-//Use one variable to represent each digit
-//Always increment the least significant digit
-//Check for carrying one digit at a time (starting with the least significant digit)
-//Check to see if you have gotten to the top of the count.  (A good way is to have an extra digit and see if you carry into it.)
-
-//Write a program to print out a 1 month calendar for any month.
-//The program should ask for the  month in a year (i.e. 3/2134)
-//The program should check to make sure the date is valid
-//The format should look like a standard calendar which shows the days of the week in 1 row and aligns the dates for each day of the week in columns.
-//This is going to take a bit of work with output formatting.
 
 int main(int argc, const char * argv[]) {
+    //Count in user-specified base
+    
+    //Ask the user for the base
+    printf("What base to use? (2-10)");
+    scanf("%i", &base);
+    
+    //Bases 2-10 are acceptable
+    if()
+    //Count from 0 to the maximum number which can be written in 4 digits.
+    //Strategy:
+    //Use one variable to represent each digit
+    //Always increment the least significant digit
+    //Check for carrying one digit at a time (starting with the least significant digit)
+    //Check to see if you have gotten to the top of the count.  (A good way is to have an extra digit and see if you carry into it.)
+    
+    //Write a program to print out a 1 month calendar for any month.
+    //The program should ask for the  month in a year (i.e. 3/2134)
+    //The program should check to make sure the date is valid
+    //The format should look like a standard calendar which shows the days of the week in 1 row and aligns the dates for each day of the week in columns.
+    //This is going to take a bit of work with output formatting.
+
     // insert code here...
     printf("Hello, World!\n");
     return 0;
